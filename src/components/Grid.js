8576cf4b927806acf44145e8a164598347f6b236
oskarhertzman <43958723+oskarhertzman@users.noreply.github.com>
2020-05-22 21:03:40 +0200

    added card addition to DOM & API

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -18,6 +18,7 @@ const ENDPOINT = "http://localhost:8090/api";
 
 export default function SpacingGrid({cards}) {
 
+  const [cardRefs, setCardRefs] = useState([]);
   const [inputRefs, setInputRefs] = useState([]);
   const [serverRef, setServerRef] = useState(false);
   const [list, setList] = useState(cards);
@@ -31,77 +32,95 @@ export default function SpacingGrid({cards}) {
 
 
   useEffect (() => {
-    console.log("hej");
     setInputRefs(inputRefs => (
       Array(list.length).fill().map((_, i) => inputRefs[i] || createRef())
     ));
+    setCardRefs(cardRefs => (
+      Array(list.length).fill().map((_, i) => cardRefs[i] || createRef())
+    ));
+  }, [list])
+
+
+  useEffect (() => {
     if (serverRef) {
       let data = JSON.stringify({
         data: list
       });
-      console.log(data);
       axios.post(ENDPOINT, data, {
         headers: {
-        'Content-Type': 'application/json; charset=UTF-8'
-      }})
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        console.log(err);
-      })
+          'Content-Type': 'application/json; charset=UTF-8'
+        }})
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        setServerRef(false);
+      }
+    }, [list, serverRef])
+
+
+    function addCard (inputRef) {
+      return inputRef.current.style.visibility = 'visible';
     }
-  }, [list, serverRef])
+
+    function newCard (e, cardRef) {
+      console.log("hej");
+      let title = e.target.value;
+      let id = cardRef.current.id;
+      let listCopy = list;
+      for (let [index,card] of list.entries()) {
+        if (card.uuid === id) {
+          card.cards.push({title: title})
+          listCopy.splice(index, card)
+          break;
+        }
+      }
+      setList([...listCopy])
+      setServerRef(true);
+    }
+
+    function newList () {
+      setOpen(true);
+    }
 
 
-  function addCard (e) {
-    return e.current.style.visibility = 'visible';
-  }
+    function handleConfirm () {
+      setList(prevState => [...prevState, {list: listName, uuid: uuidv4(), cards:[]}]);
+      setListName('');
+      setServerRef(true);
+      setOpen(false);
+    }
 
-  function newCard (e) {
-    console.log(e.target.value);
-  }
+    function handleCancel () {
+      setOpen(false);
+    };
 
-  function newList () {
-    setOpen(true);
-  }
-
-
-  function handleConfirm () {
-    setList(prevState => [...prevState, {list: listName, uuid: uuidv4(), cards:[]}]);
-    setListName('');
-    setServerRef(true);
-    setOpen(false);
-  }
-
-  function handleCancel () {
-    setOpen(false);
-  };
-
-  function onListName (e) {
-    setListName(e.target.value);
-  }
+    function onListName (e) {
+      setListName(e.target.value);
+    }
 
 
 
-  function onCardName (e) {
-    setCardName(e.target.value)
-  }
+    function onCardName (e) {
+      setCardName(e.target.value)
+    }
 
-
-
-
-console.log(list);
-
-  return (
-    <>
-    <Grid container className={paper.root} spacing={2}>
-      <Grid>
-        <Grid container className={paper.container} justify="flex-start" spacing={3}>
-          {list.map((value, index) => (
-            <Grid key={index} item>
-              <Paper className={paper.paper}>
-                <p>{value.list}</p>
+    return (
+      <>
+      <Grid container className={paper.root} spacing={2}>
+        <Grid>
+          <Grid container className={paper.container} justify="flex-start" spacing={3}>
+            {list.map((listItem, index) => (
+              <Grid key={index} ref={cardRefs[index]} id={listItem.uuid} item>
+                <Paper className={paper.paper}>
+                  <p>{listItem.list}</p>
+                  <div className={paper.cards}>
+                  {listItem.cards.map((card, index) => (
+                  <div className={paper.card} key={index}>{card.title}</div>
+                ))}
+              </div>
                 <form onSubmit={newCard} noValidate autoComplete="off">
                   <TextField
                     onChange={onCardName}
@@ -111,43 +130,43 @@ console.log(list);
                     onKeyPress={(ev) => {
                       if (ev.key === 'Enter') {
                         ev.preventDefault();
-                        newCard(ev);
+                        newCard(ev, cardRefs[index]);
                       }
                     }} />
-                </form>
-                <Button onClick={ () => addCard(inputRefs[index])} className={paper.cardButton}>Add new card</Button>
-              </Paper>
-            </Grid>
-          ))}
-          <Button onClick={newList} className={paper.listButton}>Add new list</Button>
+                  </form>
+                  <Button onClick={ () => addCard(inputRefs[index])} className={paper.cardButton}>Add new card</Button>
+                </Paper>
+              </Grid>
+            ))}
+            <Button onClick={newList} className={paper.listButton}>Add new list</Button>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
-    <Dialog open={open} onClose={handleCancel || handleConfirm} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">New list</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Give your list a productive title that represents it's future context.
-        </DialogContentText>
-        <TextField
-          onChange={onListName}
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Title"
-          type="text"
-          fullWidth
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleCancel} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={handleConfirm} color="primary">
-          Enter
-        </Button>
-      </DialogActions>
-    </Dialog>
-  </>
-);
+      <Dialog open={open} onClose={handleCancel || handleConfirm} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">New list</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Give your list a productive title that represents it's context.
+          </DialogContentText>
+          <TextField
+            onChange={onListName}
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Title"
+            type="text"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancel} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm} color="primary">
+            Enter
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
